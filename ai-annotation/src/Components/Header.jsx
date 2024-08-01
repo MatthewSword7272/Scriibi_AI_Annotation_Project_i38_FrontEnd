df12561340b94691eb84904cea07e17c8ada5269
Matthew Catalfamo <sword1005@hotmail.com.au>
2024-08-02 09:30:29 +1000

    Fixed Header Compoent

--- ai-annotation/src/Components/Header.jsx
+++ ai-annotation/src/Components/Header.jsx
@@ -1,22 +1,30 @@
 import React from "react";
-import { StyledHeader, StyledHeaderSection, StyledHeaderLogo, StyledHeaderLinks, StyledHeaderProfile } from '../Styles/StyledHeader';
+import {
+  StyledHeader,
+  StyledHeaderSection,
+  StyledHeaderLogo,
+  StyledHeaderLinks,
+  StyledHeaderProfile,
+} from "../Styles/StyledHeader";
 
 function Header() {
+  return (
     <StyledHeader>
       <StyledHeaderSection>
-          <StyledHeaderLogo src={'scriibi-logo.png'}/>
-          <StyledHeaderLinks>
-            <h4>HOME</h4>
-            <h4>LESSONS</h4>
-            <h4>DATA</h4>
-            <h4>ASSESSMENTS</h4>
-          </StyledHeaderLinks>
-        </StyledHeaderSection>
-        <StyledHeaderProfile>
-          <span>David Nicolaides</span>
-          <span className="e-icons e-settings"></span>
-        </StyledHeaderProfile>
-      </StyledHeader>
+        <StyledHeaderLogo src={"scriibi-logo.png"} />
+        <StyledHeaderLinks>
+          <h4>HOME</h4>
+          <h4>LESSONS</h4>
+          <h4>DATA</h4>
+          <h4>ASSESSMENTS</h4>
+        </StyledHeaderLinks>
+      </StyledHeaderSection>
+      <StyledHeaderProfile>
+        <span>David Nicolaides</span>
+        <span className="e-icons e-settings"></span>
+      </StyledHeaderProfile>
+    </StyledHeader>
+  );
 }
 
 export default Header;
