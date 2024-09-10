7ccf18378098cbb85e9b6a141b3f9c3cd2ecccdc
Matthew Catalfamo <sword1005@hotmail.com.au>
2024-09-10 22:09:08 +1000

    fix: Removed border and changed box shadow

--- ai-annotation/src/Styles/StyledDialogBox.js
+++ ai-annotation/src/Styles/StyledDialogBox.js
@@ -1,26 +1,17 @@
 import { DialogComponent } from "@syncfusion/ej2-react-popups";
-import { GREY } from "Constraints/constants";
 import styled from "styled-components";
 
 const StyledDialogBox = styled(DialogComponent)`
 
     border-radius: 15px;
+    box-shadow: 0px 8px 10px 0px rgba(0, 0, 0, .5);
 
     .e-dlg-header-content {
         border-radius: 15px 15px 0 0; // Matching top corners
-        border: ${GREY} 3px solid;
-        border-bottom: 0;
-    }
-
-    
-    .e-dlg-content {
-        border-left: ${GREY} 3px solid;
-        border-right: ${GREY} 3px solid;
     }
 
    .e-dialog-border-resize .e-south {
         border-radius: 0 0 15px 15px; // Matching top corners
-        border-bottom: ${GREY} 3px solid;
     }
 `
 
